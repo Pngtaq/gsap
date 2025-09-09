2195e200eb35c22c0dfe3028d5773f2e240e1330
RaisonSalvador <raisonsalvador0@gmail.com>
2025-09-09 23:12:40 +0800

    feat: hero section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
+// "use client";
 import React from "react";
 //GSAP
-import { ScrollTrigger, SplitText } from "gsap/all";
+// import { ScrollTrigger, SplitText } from "gsap/all";
 //components
 import Navbar from "@/components/Navbar";
 import gsap from "gsap";
-
-gsap.registerPlugin(ScrollTrigger, SplitText);
+import Hero from "@/components/Hero";
 
 const page = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
     </main>
   );
 };
